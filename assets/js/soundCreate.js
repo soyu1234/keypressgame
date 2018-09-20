5f42e2b8721f5751b39f063d9d92a37f887e90d2
soyu1234 <smileprew@gmail.com>
2018-09-20 12:56:08 +0300

    added sounds effects to uppercase letters

--- assets/js/soundCreate.js
+++ assets/js/soundCreate.js
@@ -1,14 +1,15 @@
 var sound;
         var i=97;
+        var j=65;
         var sArr=['bubbles','clay','confetti','corona','dotted-spiral','flash-1','flash-2','flash-3','glimmer','moon','pinwheel','piston-1','piston-2','piston-3','prism-1','prism-2','prism-3','splits','squiggle','strike','suspension','timer','ufo','veil','wipe','zig-zag'];
         document.addEventListener('keypress', function(e) {
             for(var a=0;a<26;a++){
-                if(e.which===i){
+                if(e.which===i || e.which===j){
                     sound=soundCreate(sArr[a]);
                     sound.play();
                     break;
                 }
-                else if(e.which===i+a){
+                else if(e.which===i+a || e.which===j+a){
                     sound=soundCreate(sArr[a]);
                     sound.play();
                     break;
